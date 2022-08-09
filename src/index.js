c6af84754ee9329486da2601ddef168aba45cef2
Md Nuruzzaman Faruk <mdnfaruk.fin@gmail.com>
2022-08-10 02:04:04 +0300

    changed endponit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+/* import Todo from './Todo'
+import App from './App';
+import Count from './Count';
+import Calculator from './Ex_5_1';
+import Nasa from './Nasa';
+import UserList from './UserList'
+import QuestionApi from './QuestionApi'
+import Githubapi from './githubapi2'
+import TodoOutcomponent from './Todo_Out_component'
+import MaterialTodo from './MaterialUiTodo'
+import Firebasetodo from './Firebasetodo'*/
+import Bookstore from './Bookstore'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Bookstore/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
